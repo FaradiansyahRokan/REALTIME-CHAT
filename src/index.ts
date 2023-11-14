d104b07e63ee6e6b5ebebad81e92a6a118c64332
rokan <rokansubhi.f@gmail.com>
2023-11-14 13:48:27 +0700

    port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 import {Server} from 'socket.io'
 import http from 'http'
 
-const port = 5000
+const port = 3306
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
